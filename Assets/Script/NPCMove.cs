ce6db0f8e6b6f4b436dcaa3ea5f582aa6f19096a
Sof <misscat066@gmail.com>
2025-06-13 04:18:45 +0800

    add ui ux

--- Assets/Script/NPCMove.cs
+++ Assets/Script/NPCMove.cs
@@ -26,7 +26,7 @@ public class NPCMove : MonoBehaviour
     {
         float distToTarget = Vector3.Distance(targets[targetIndex].position, transform.position);
         agent.destination = targets[targetIndex].position;
-        if (distToTarget < 0.5f)
+        if (distToTarget < 0.7f)
         {
             anim.SetBool("isWalking", false);
             countUp += Time.deltaTime;
@@ -49,3 +49,5 @@ public class NPCMove : MonoBehaviour
         }
     }
 }
+
+
