336e2d00572579254f80b571777af82a123a8a9a
Sof <misscat066@gmail.com>
2025-06-18 02:40:28 +0800

    win point

---
+++ Assets/Script/WinCoins.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class WinCoins : MonoBehaviour
+{
+    public CoinCollector coinCollector; // Drag your Player's CoinCollector script here
+    public int requiredCoins = 21;
+
+    public GameObject bossNPC; // NPC or enemy to activate after collecting all coins
+    public GameObject infoText; // Optional: show "You’re ready" text
+
+    private bool hasTriggered = false;
+
+    void Update()
+    {
+        if (!hasTriggered && coinCollector.coinCount >= requiredCoins)
+        {
+            hasTriggered = true;
+
+            if (bossNPC != null)
+                bossNPC.SetActive(true); // Activate boss or next challenge
+
+            if (infoText != null)
+                infoText.SetActive(true); // Optional UI that says "Prepare to fight"
+
+            Debug.Log("Player collected all coins. Boss fight can begin!");
+        }
+    }
+}
