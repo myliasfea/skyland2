cebcfb47acf0e0413235d3c8d3f1e30ec8c9bf8c
Sof <misscat066@gmail.com>
2025-06-18 02:08:25 +0800

    text

--- Assets/Script/PortalTeleport.cs
+++ Assets/Script/PortalTeleport.cs
@@ -2,14 +2,23 @@ using UnityEngine;
 
 public class PortalTeleport : MonoBehaviour
 {
-    public Transform teleportTarget; // Target location to teleport to
+    public Transform teleportTarget; // Assign this in Inspector
+    public string playerTag = "Player"; // Make sure your player is tagged "Player"
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player")) // Make sure your player is tagged as Player
+        if (other.CompareTag(playerTag))
         {
+            CharacterController cc = other.GetComponent<CharacterController>();
+            if (cc != null)
+                cc.enabled = false;
+
             other.transform.position = teleportTarget.position;
-            Debug.Log("Teleported to: " + teleportTarget.name);
+
+            if (cc != null)
+                cc.enabled = true;
+
+            Debug.Log("Player teleported to " + teleportTarget.position);
         }
     }
 }
---
+++ Assets/Script/WinPoint.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class WinPoint : MonoBehaviour
+{
+    public GameObject winUI; // Optional: assign a UI panel or text in Inspector
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Player wins the game!");
+
+            if (winUI != null)
+                winUI.SetActive(true); // Show Win message
+
+            // Optional: freeze time
+            Time.timeScale = 0f;
+
+            // OR: Load another scene if you want
+            // SceneManager.LoadScene("WinScene");
+        }
+    }
+}
